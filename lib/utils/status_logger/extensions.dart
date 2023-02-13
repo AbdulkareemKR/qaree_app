52b172f9fd9ce67ac9e731382aed9c6f176ca4f5
Abdulkareeem Alramlawi <76615134+AbdulkareemKR@users.noreply.github.com>
2023-02-13 16:31:34 +0300

    new: create status logger

---
+++ lib/utils/status_logger/extensions.dart
@@ -0,0 +1,10 @@
+import 'package:Naqaa/utils/status_logger/status_logger.dart';
+
+extension ExceptionLogger on Object {
+  /// An easy way to log the exception.
+  void logException({StackTrace? stackTrace}) {
+    if (this is Error || this is Exception) {
+      StatusLogger.error('$this', stackTrace: stackTrace);
+    }
+  }
+}
